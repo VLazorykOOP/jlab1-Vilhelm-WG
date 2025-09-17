3266d4f0ad3e51301c1031d6e78e22af0266fd82
Vilhelm-WG <haisiuk.vilhelm@chnu.edu.ua>
2025-09-17 13:59:31 +0300

    work commit

--- src/task1.java
+++ src/task1.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class task1 {
+    public static void main(String[] args) {
+        System.out.print("Enter x and y: ");
+        Scanner in = new Scanner(System.in);
+        float s = in.nextFloat();
+        double p = 4 * Math.sqrt(s);
+        System.out.println("p=" + p);
+        in.close();
+    }
 }
